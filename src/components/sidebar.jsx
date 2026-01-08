27eddf108ccd06e15819af53f6a76c5f510b855d
Dharmendra Gora <dharmgora.23@gmail.com>
2026-01-08 23:11:53 +0530

    update the css for UI

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -5,7 +5,7 @@ const sidebar = ({ selectedTab, setSelectTab }) => {
   return (
     <div
       className="d-flex flex-column flex-shrink-0 p-3 text-bg-dark sidebar"
-      style={{ width: "280px" }}
+      style={{ width: "240px" }}
     >
       <a
         href="/"
